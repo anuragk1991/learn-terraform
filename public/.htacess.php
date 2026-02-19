ed0fc2e4ec5448c284b3db1d960356d39a34ee26
Anurag Kumar <anurag.kumar@cartrack.com>
2026-02-19 22:31:27 +0530

    Initial PHP demo app

---
+++ public/.htacess.php
@@ -0,0 +1,4 @@
+DirectoryIndex index.php
+
+# Not strictly needed with separate about.php, but fine to keep
+RewriteEngine On
---
+++ public/about.php
@@ -0,0 +1,17 @@
+<?php
+?><!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>About Us - Demo App</title>
+</head>
+<body>
+    <nav>
+        <a href="/">Home</a> |
+        <a href="/about.php">About Us</a>
+    </nav>
+    <hr>
+    <h1>About Us</h1>
+    <p>We are learning how to do proper CI/CD with PHP + AWS.</p>
+</body>
+</html>
