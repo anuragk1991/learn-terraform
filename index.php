c9962e4b32f47d2e45d0d414d1e885352d48e8df
Anurag Kumar <anurag.kumar@cartrack.com>
2026-02-19 22:46:43 +0530

    Test commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+?><!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Home - Demo App1</title>
+</head>
+<body>
+    <nav>
+        <a href="/">Home</a> |
+        <a href="/about.php">About Us</a>
+    </nav>
+    <hr>
+    <h1>Homepage</h1>
+    <p>This is a simple PHP demo app used for CI/CD + Terraform training.</p>
+</body>
+</html>
